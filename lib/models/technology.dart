613a734d07833223c9d730db54a05eb26508ee61
Chandra <ardhyachandrax@gmail.com>
2024-04-06 11:19:43 +0700

    change new var

--- lib/models/technology.dart
+++ lib/models/technology.dart
@@ -11,22 +11,27 @@ class TechnologyConstants {
   static TechnologyModel java = TechnologyModel("Java", AppConstants.javaImage);
   static TechnologyModel golang =
       TechnologyModel("Golang", AppConstants.golangImage);
-  static TechnologyModel laravel =
-      TechnologyModel("Laravel", AppConstants.laravelImage);
   static TechnologyModel flutter =
       TechnologyModel("Flutter", AppConstants.flutterImage);
-  static TechnologyModel mysql =
-      TechnologyModel("MySQL", AppConstants.mysqlImage);
+  static TechnologyModel kotlin =
+      TechnologyModel("Kotlin", AppConstants.kotlinImage);
+  static TechnologyModel reactNat =
+      TechnologyModel("ReactNative", AppConstants.reactNatImage);
   static TechnologyModel firebase =
       TechnologyModel("Firebase", AppConstants.firebaseImage);
-  static TechnologyModel php = TechnologyModel("PHP", AppConstants.phpImage);
+  static TechnologyModel javascript =
+      TechnologyModel("Javascript", AppConstants.javascriptImage);
+  static TechnologyModel html = TechnologyModel("HTML", AppConstants.htmlImage);
+  static TechnologyModel css = TechnologyModel("CSS", AppConstants.cssImage);
 
   static List<TechnologyModel> technologyLearned = [
     flutter,
+    kotlin,
+    reactNat,
     java,
-    mysql,
     firebase,
-    laravel,
-    php
+    javascript,
+    html,
+    css,
   ];
 }
